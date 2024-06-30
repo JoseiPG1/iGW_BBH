19c02203a0d634d1aeaf62aed90c2a9f61199277
JoseiPG1 <jose.perdiguerog@gmail.com>
2024-06-30 16:45:04 -0400

    Updating computations

--- Extreme_Mass_Ratio/models.jl
+++ Extreme_Mass_Ratio/models.jl
@@ -1,13 +1,4 @@
 function NewtonianOrbitModel(u, model_params, t)
-    #=
-        Defines system of odes which describes motion of
-        point like particle with Newtonian physics, uses
-
-        u[1] = χ
-        u[2] = ϕ
-
-        where, p, M, and e are constants
-    =#
     χ, ϕ = u
     p, M, e  = model_params
 
@@ -22,15 +13,6 @@ function NewtonianOrbitModel(u, model_params, t)
 end
 
 function RelativisticOrbitModel(u, model_params, t)
-    #=
-        Defines system of odes which describes motion of
-        point like particle in schwarzschild background, uses
-
-        u[1] = χ
-        u[2] = ϕ
-
-        where, p, M, and e are constants
-    =#
     χ, ϕ = u
     p, M, e  = model_params
 
